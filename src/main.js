6864b6e9e5091a4ddb49898a96fccb99a4b5b087
GiPisano <pisanogiacomo9@gmail.com>
2024-05-08 18:11:48 +0200

    init vue and install axios, router, bootsrap and fontawesome.

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as bootstrap from "bootstrap";
+
+// fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// import { faYouTube, faFacebook } from 'fortawesome/free-brands-svg-icons';
+// library.add(faYouTube, faFacebook);
+//nel caso in cui si voglia scaricare la stessa icona ma piena (fa-solid) e vuota (fa-regular), dovrò rinominarla perchè altrimenti darà errore, quindi ad esempio scrivo:
+// import { faStar as faStarSolid } from "fortawesome/free-solid-svg-icons";
+
+// esempio
+// <font-awesome-icon icon="fa-solid fa-house" />
+
+import { router } from "./router";
+
+const app = createApp(App);
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
