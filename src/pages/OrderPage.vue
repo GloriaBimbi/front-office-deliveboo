762432c0de3f6f569d2f854f75427b49ccabb855
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-05-19 16:41:55 +0200

    feat: style order page

--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -60,104 +60,170 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <h2>Your Orders</h2>
+  <div class="container-fluid">
+    <!-- go back to homepage on click -->
+    <router-link :to="{ name: 'home' }" class="btn back-button mb-2">
+      <i class="fa-solid fa-arrow-rotate-left"></i>
+      <span class="back-button-label"> Back to Home</span>
+    </router-link>
+    <div class="card mb-3">
+      <h2>Your Orders</h2>
 
-    <div class="row" v-if="this.currentOrders.length">
-      <div
-        class="col-12 order-details"
-        v-for="(order, index) in this.currentOrders"
-        :key="index"
-      >
-        <div>
-          <p>Status: {{ order.status }}</p>
-          <p>Message: {{ order.message }}</p>
-          <ul class="cart-list">
-            <li
-              class="cart-list-item"
-              v-for="(item, itemIndex) in order.items"
-              :key="itemIndex"
-            >
-              <div class="img-dish-wrapper">
-                <img :src="item.image" alt="" />
+      <div class="row" v-if="this.currentOrders.length">
+        <div
+          class="order-details"
+          v-for="(order, index) in this.currentOrders"
+          :key="index"
+        >
+          <div>
+            <p>Status: {{ order.status }}</p>
+            <p>Message: {{ order.message }}</p>
+            <ul class="cart-list">
+              <li
+                class="cart-list-item"
+                v-for="(item, itemIndex) in order.items"
+                :key="itemIndex"
+              >
+                <div class="img-dish-wrapper">
+                  <img :src="item.image" alt="" />
+                </div>
+                <div class="primary-info">
+                  <h5>{{ item.name }}</h5>
+                  <p>price: {{ formatPrice(item.price) }}</p>
+                </div>
+                <div class="quantity-info">
+                  <h4 class="mt-auto">x {{ item.quantity }}</h4>
+                </div>
+              </li>
+            </ul>
+            <div class="checkout-wrapper mt-auto">
+              <div class="total-price d-flex align-items-center gap-3">
+                <h4>Total price:</h4>
+                <span> {{ order.totalPrice }}</span>
               </div>
-              <div class="primary-info">
-                <h5>{{ item.name }}</h5>
-                <p>price: {{ formatPrice(item.price) }}</p>
-              </div>
-              <div class="quantity-info">
-                <h4 class="mt-auto">x {{ item.quantity }}</h4>
-              </div>
-            </li>
-          </ul>
-          <div class="checkout-wrapper mt-auto">
-            <div class="total-price d-flex align-items-center gap-3">
-              <h4>Total price:</h4>
-              <span> {{ order.totalPrice }}</span>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <p>You have no current orders.</p>
+      <div v-else>
+        <p class="text-info">You have no current orders.</p>
+      </div>
     </div>
 
-    <h2>Your Order History</h2>
-    <div class="row" v-if="this.ordersHistory.length">
-      <div
-        class="col-12 order-details"
-        v-for="(order, index) in ordersHistory"
-        :key="index"
-      >
-        <div>
-          <h3>{{ order.items[0].restaurant }}</h3>
-          <p>Order Placed - {{ formatDate(order.date) }}</p>
-          <ul class="cart-list">
-            <li
-              class="cart-list-item"
-              v-for="(item, itemIndex) in order.items"
-              :key="itemIndex"
-            >
-              <div class="img-dish-wrapper">
-                <img :src="item.image" alt="" />
+    <div class="card">
+      <h2>Your Order History</h2>
+      <div v-if="this.ordersHistory.length">
+        <div class="row d-flex flex-column-reverse">
+          <div
+            class="col-12 order-details"
+            v-for="(order, index) in ordersHistory"
+            :key="index"
+          >
+            <div>
+              <h3>{{ order.items[0].restaurant }}</h3>
+              <p>Order Placed - {{ formatDate(order.date) }}</p>
+              <ul class="cart-list">
+                <li
+                  class="cart-list-item"
+                  v-for="(item, itemIndex) in order.items"
+                  :key="itemIndex"
+                >
+                  <div class="img-dish-wrapper">
+                    <img :src="item.image" alt="" />
+                  </div>
+                  <div class="primary-info">
+                    <h5>{{ item.name }}</h5>
+                    <p>price: {{ formatPrice(item.price) }}</p>
+                  </div>
+                  <div class="quantity-info">
+                    <h4 class="mt-auto">x {{ item.quantity }}</h4>
+                  </div>
+                </li>
+              </ul>
+              <div class="checkout-wrapper mt-auto">
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="total-price d-flex align-items-center gap-3">
+                    <h4>Total price:</h4>
+                    <span class="price">{{ order.totalPrice }}</span>
+                  </div>
+                </div>
               </div>
-              <div class="primary-info">
-                <h5>{{ item.name }}</h5>
-                <p>price: {{ formatPrice(item.price) }}</p>
-              </div>
-              <div class="quantity-info">
-                <h4 class="mt-auto">x {{ item.quantity }}</h4>
-              </div>
-            </li>
-          </ul>
-          <div class="checkout-wrapper mt-auto">
-            <div class="total-price d-flex align-items-center gap-3">
-              <h4>Total price:</h4>
-              <span> {{ order.totalPrice }}</span>
             </div>
           </div>
         </div>
+        <button
+          class="btn btn-link text-danger text-end"
+          @click="clearOrderHistory"
+        >
+          Clear Order History
+        </button>
       </div>
-      <button
-        class="btn btn-link text-danger text-end"
-        @click="clearOrderHistory"
-      >
-        Clear Order History
-      </button>
-    </div>
 
-    <div v-else>
-      <p>No previous orders.</p>
+      <div v-else>
+        <p class="text-info">No previous orders.</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 * {
-  color: white;
+  color: black;
+}
+.container-fluid {
+  position: relative;
+  padding-block: 5rem;
+}
+
+.back-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+  position: absolute;
+  left: 0px;
+  top: 10px;
+  width: 60px;
+  aspect-ratio: 1 / 1;
+  background-color: rgba(255, 255, 255, 0.807);
+  font-weight: bold;
+  border-radius: 0px;
+  z-index: 1;
+  opacity: 0.5;
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+  .back-button-label {
+    display: none;
+  }
+  i {
+    display: block;
+  }
+
+  &:hover {
+    opacity: 1;
+    width: 180px;
+    aspect-ratio: 3 / 1;
+    z-index: 2;
+    transition: opacity linear 0.3s;
+    .back-button-label {
+      display: block;
+    }
+  }
+}
+
+.card {
+  background-color: white;
+  padding: 1rem;
+  border-radius: 2px;
 }
 .order-details {
+  border-bottom: 2px solid #0073de;
+  padding-block: 1rem;
+  &:first-of-type {
+    border-bottom: none;
+  }
   .cart-list {
     max-height: 700px;
     overflow: auto;
@@ -195,7 +261,7 @@ export default {
         flex-direction: column;
         justify-content: space-between;
         h4 {
-          color: white;
+          // color: white;
           font-weight: medium;
         }
       }
@@ -203,6 +269,17 @@ export default {
   }
   .checkout-wrapper {
     padding: 0.5rem;
+    .total-price {
+      line-height: 1rem;
+      h4 {
+        margin-bottom: 0;
+      }
+      .price {
+        height: 1rem;
+        vertical-align: top;
+        font-size: 1.5rem;
+      }
+    }
   }
 }
 </style>
